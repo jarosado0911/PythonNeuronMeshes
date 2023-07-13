24bb487067fe299f2a6f6d2bd2ec910e6fc70fef
CrypticInventions <bhugger02@gmail.com>
2023-07-13 03:19:40 -0400

    Create Interface.py

---
+++ Interface.py
@@ -0,0 +1,4 @@
+import subprocess
+import os
+os.system('wsl  -e sh -c "ls -l > cd /mnt/c/Users/bhugg/documents/GitHub/PythonNeuronMeshes; python3 NEW_generate_meshes.py -i cells/228-21MG.CNG.swc')
+#os.rename("C:\\Users\\bhugg\\Documents\\GitHub\\PythonNeuronMeshes\\out.vrn", "C:\\Users\\bhugg\\Documents\\GitHub\\Neuro-VISOR\\Assets\\streamingAssets\\NeuronalDynamics\\Geometries\\out.vrn")
